30a376fe3180808ccb812baa4c8dc884144a264a
charlie-react <105398445+charlie-react@users.noreply.github.com>
2022-10-20 08:00:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const closeModalButton = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+const buttons = document.querySelectorAll('.show-modal');
+
+const showModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', showModal);
+}
+
+closeModalButton.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (y) {
+  if (y.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
